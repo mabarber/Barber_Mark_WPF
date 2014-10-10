a8a469d5925ccb9ac3626846ff1c2589ab5a5bf9
Mark Barber <mabarber@fullsail.edu>
2014-10-09 22:31:45 -0400

    Wacky

---
+++ Expressions_Wacky/Expression_Wacky.js
@@ -0,0 +1,28 @@
+ /*
+Mark Barber
+WPF 1408 Section 02
+Expressions
+10-09-2014
+ */
+ // "Should I wear my jacket in Canada today?"
+
+ <script type="text.javascript">
+
+ // Name variables and prompt for known variable input
+
+ var degCels = prompt("Enter Temperature in Celsius",18);
+ var degFahren;
+
+ // Label Equation for Calculation: F = (C x 9/5) + 32
+
+ degFahren = (degCels * 9/5) + 32
+
+ // I don't care what the temperature actually is, just if I should wear my jacket or not, and I know I am comfortable at 60+ degrees Fahrenheit
+ //Add if/then statement and appropriate alert text
+
+ if (degFahren <60)
+     alert ("Wear your jacket today");
+
+ else {
+     alert("Do not wear your jacket today.");
+     }
--- Go To Training Week 2/js/week2.js
+++ Go To Training Week 2/js/week2.js
@@ -2,7 +2,7 @@
 Mark Barber
 WPF 1408 Section 02
 Go To Training Week 2
-10-08-2014
+10-09-2014
  */
 
 //alert("Testing 1,2,3");
